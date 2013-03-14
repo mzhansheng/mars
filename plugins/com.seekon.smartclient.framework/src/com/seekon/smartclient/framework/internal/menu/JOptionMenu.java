b5887ce9265520a797b2bcf0c20ed7012460de61
liuxiaoyong <undyliu@126.com>
2013-03-14 21:33:45 +0800

    增加swing皮肤

--- plugins/com.seekon.smartclient.framework/src/com/seekon/smartclient/framework/internal/menu/JOptionMenu.java
+++ plugins/com.seekon.smartclient.framework/src/com/seekon/smartclient/framework/internal/menu/JOptionMenu.java
@@ -28,14 +28,40 @@ public class JOptionMenu extends JMenu {
   public JOptionMenu() {
     super();
 
-    LAFS = new LAF[3];
+    LAFS = new LAF[16];
     LAFS[0] = new LAF("Metal", "javax.swing.plaf.metal.MetalLookAndFeel",
       LAF.THEME_DEAULT);
     LAFS[1] = new LAF("Windows",
       "com.sun.java.swing.plaf.windows.WindowsLookAndFeel", LAF.THEME_DEAULT);
     LAFS[2] = new LAF("Nimbus", "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel",
       LAF.THEME_DEAULT);
-
+    LAFS[3] = new LAF("Acryl", "com.jtattoo.plaf.acryl.AcrylLookAndFeel",
+      LAF.THEME_DEAULT);
+    LAFS[4] = new LAF("Aero", "com.jtattoo.plaf.aero.AeroLookAndFeel",
+      LAF.THEME_DEAULT);
+    LAFS[5] = new LAF("Aluminium", "com.jtattoo.plaf.aluminium.AluminiumLookAndFeel",
+      LAF.THEME_DEAULT);
+    LAFS[6] = new LAF("Bernstein", "com.jtattoo.plaf.bernstein.BernsteinLookAndFeel",
+      LAF.THEME_DEAULT);
+    LAFS[7] = new LAF("Fast", "com.jtattoo.plaf.fast.FastLookAndFeel",
+      LAF.THEME_DEAULT);
+    LAFS[8] = new LAF("Graphite", "com.jtattoo.plaf.graphite.GraphiteLookAndFeel",
+      LAF.THEME_DEAULT);
+    LAFS[9] = new LAF("HiFi", "com.jtattoo.plaf.hifi.HiFiLookAndFeel",
+      LAF.THEME_DEAULT);
+    LAFS[10] = new LAF("Luna", "com.jtattoo.plaf.luna.LunaLookAndFeel",
+      LAF.THEME_DEAULT);
+    LAFS[11] = new LAF("McWin", "com.jtattoo.plaf.mcwin.McWinLookAndFeel",
+      LAF.THEME_DEAULT);
+    LAFS[12] = new LAF("Mint", "com.jtattoo.plaf.mint.MintLookAndFeel",
+      LAF.THEME_DEAULT);
+    LAFS[13] = new LAF("Noire", "com.jtattoo.plaf.noire.NoireLookAndFeel",
+      LAF.THEME_DEAULT);
+    LAFS[14] = new LAF("Smart", "com.jtattoo.plaf.smart.SmartLookAndFeel",
+      LAF.THEME_DEAULT);
+    LAFS[15] = new LAF("Texture", "com.jtattoo.plaf.texture.TextureLookAndFeel",
+      LAF.THEME_DEAULT);
+    
     this.setText(ResourceProvide.getResourceString(OPTION_MENU_KEY + "menu.text"));
     this.setMnemonic(ResourceProvide.getMnemonic(OPTION_MENU_KEY + "menu.mnemonic"));
 
@@ -118,6 +144,7 @@ public class JOptionMenu extends JMenu {
       Application application = (Application) Activator.getBundleContext()
         .getService(sr);
       try {
+        UIManager.put("ClassLoader", laf.getLaf().getClass().getClassLoader());
         application.changeLookAndFeel(laf.getClassName());
       } catch (Exception e) {
         e.printStackTrace();
