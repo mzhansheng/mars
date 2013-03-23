59f407e6aecbeafe90040a355e3ed83d3e21ee3a
liuxiaoyong <undyliu@126.com>
2013-03-23 09:16:55 +0800

    打开界面增加等待信息

--- plugins/com.seekon.smartclient.framework/src/com/seekon/smartclient/framework/nav/NavigationItemDoubleClickedListener.java
+++ plugins/com.seekon.smartclient.framework/src/com/seekon/smartclient/framework/nav/NavigationItemDoubleClickedListener.java
@@ -1,15 +1,19 @@
 package com.seekon.smartclient.framework.nav;
 
+import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreePath;
 
 import com.javadocking.dock.Position;
+import com.seekon.smartclient.common.DefaultInvokeHandler;
+import com.seekon.smartclient.common.UIUtilities;
 import com.seekon.smartclient.framework.Application;
 import com.seekon.smartclient.framework.internal.Activator;
 import com.seekon.smartclient.module.AbstractResource;
@@ -41,16 +45,38 @@ public class NavigationItemDoubleClickedListener extends MouseAdapter {
 				  return;
 				}
 				AbstractResource item = (AbstractResource) node.getUserObject();
-				Component compo = navigation.getComponent(path);
-				if (compo == null) {
-					compo = new JLabel(item.getName() + "正在开发中");
-				}
 				application
 				.getMainFrame()
 				.getCurrentPerspective()
-				.addComponent(item.getId(), compo, item.getName(), new Position());
+				.addComponent(item.getId(), new JPanelWrapper(path), item.getName(), new Position());
 			}
 		}
 
+	}
+	
+	class JPanelWrapper extends JPanel{
+    private static final long serialVersionUID = -2986321563725084028L;
+
+    public JPanelWrapper(final TreePath path) {
+      super();
+      this.setLayout(new BorderLayout());
+      
+      UIUtilities.asyncInvoke( new DefaultInvokeHandler<Component>() {
+
+        @Override
+        public Component execute() throws Exception {
+          return navigation.getComponent(path);
+        }
+
+        @Override
+        public void success(Component result) {
+          if (result == null) {
+            result = new JLabel("组件正在开发中");
+          }
+          JPanelWrapper.this.add(result);
+        }
+      });
+    }
+	  
 	}
 }
