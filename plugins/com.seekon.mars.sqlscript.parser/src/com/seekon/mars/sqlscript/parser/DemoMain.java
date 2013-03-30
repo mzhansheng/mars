f6f04d9c992e0b5140d3df8e1c010e6ed2f46d5e
liuxiaoyong <undyliu@126.com>
2013-03-30 16:44:42 +0800

    add

---
+++ plugins/com.seekon.mars.sqlscript.parser/src/com/seekon/mars/sqlscript/parser/DemoMain.java
@@ -0,0 +1,25 @@
+package com.seekon.mars.sqlscript.parser;
+
+import org.antlr.runtime.ANTLRStringStream;
+import org.antlr.runtime.TokenRewriteStream;
+
+public class DemoMain {
+  public static void main(String[] args) {
+
+  //String sql = " select * from as_user where user_id = '2' and (user_name like '%66' or user_name like '777%')";
+  String sql = " select user_name, sex from as_user u join as_emp e on u.user_id = e.emp_code where u.user_id = '@@@'" +
+  		" or u.user_id in (select id from as_ss)";
+  SQLLexer lexer = new SQLLexer(new ANTLRStringStream(sql));
+  TokenRewriteStream tokenStream = new TokenRewriteStream(lexer);
+  SQLParser parser = new SQLParser(tokenStream);
+  
+  try {
+    Object tree = parser.select_statement().getTree();
+    tokenStream.insertAfter(2, "sss");
+    System.out.println(tree);
+  } catch (Exception e) {
+    // TODO Auto-generated catch block
+    e.printStackTrace();
+  }
+  }
+}
